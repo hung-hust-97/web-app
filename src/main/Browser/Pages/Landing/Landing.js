3f6137810cb7ebc308c792b9116e19e54cb9f8be
datkieuptit <datkieuptit@gmail.com>
2025-04-23 08:50:00 +0700

    test 2

--- src/main/Browser/Pages/Landing/Landing.js
+++ src/main/Browser/Pages/Landing/Landing.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React from "react";
 import classes from "./Landing.module.css";
 import {images} from "../../../../assets/images";
 import MarketTitle from "./components/MarketTitle/MarketTitle";
@@ -8,28 +8,38 @@ import MarketView from "./components/MarketView/MarketView";
 import MarketInfo from "./components/MarketInfo/MarketInfo";
 import Footer from "../UserPanel/Sections/Footer/Footer";
 import ScrollBar from "../../../../components/ScrollBar";
+import { useSelector } from "react-redux";
 
 const Landing = () => {
-
-    return (
-        <div className={`${classes.container} move-image`} style={{backgroundImage: `url("${images.spaceStar}")`}}>
-            <div className={`row jc-between ai-center width-90 m-auto`} style={{height: "70vh"}}>
-                <div className={`width-30 column jc-between ai-start`} style={{height: "55vh"}}>
-                    <MarketTitle/>
-                    <GeneralInfo/>
-                </div>
-                <div className={`width-40 height-100`}>
-                    <Spinner/>
-                </div>
-                <div className={`width-30 flex jc-end`}>
-                    <MarketView/>
-                </div>
-            </div>
-            <div className={`flex jc-center mb-5`} style={{height: "", backgroundColor: "var(--mainContent)"}}>
-                <MarketInfo/>
-            </div>
+  return (
+    <div
+      className={`${classes.container} move-image`}
+      style={{backgroundImage: `url("${images.spaceStar}")`}}>
+      <div
+        className={`row jc-between ai-center width-90 m-auto`}
+        style={{height: "55vh"}}>
+        <div
+          className={`width-30 column jc-between ai-start`}
+          style={{height: "55vh"}}>
+          <MarketTitle />
+          <GeneralInfo />
         </div>
-    );
-}
+        <div className={`width-40  jc-end`}>
+          <Spinner />
+        </div>
+      </div>
+      <div
+        className={`flex jc-center mb-5`}
+        style={{height: "", backgroundColor: "var(--mainContent)"}}>
+        <MarketView />
+      </div>
+      <div
+        className={`flex jc-center mb-5`}
+        style={{height: "", backgroundColor: "var(--mainContent)"}}>
+        <MarketInfo />
+      </div>
+    </div>
+  );
+};
 
 export default Landing;
